a339b2af8b54e4b913230a5fb2ef0c5bc1e81bd8
softramesh29 <softramesh29@gmail.com>
2022-04-16 09:50:52 +0530

    Instead of IF condition Swich case used

---
+++ AddparttimeUC4.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.EmpWageBuilder_Day4;
+
+public class AddparttimeUC4 {
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
+    static final int WAGE_PER_HRS = 20;
+    
+    public static void main(String[] args) {
+
+    	int empHrs = 0;
+    	int empWage = 0;
+
+    	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+    	switch (empCheck) {
+		case IS_FULL_TIME:
+		empHrs = 8;
+		break;
+		case IS_PART_TIME:
+		empHrs = 8;
+		break;
+		default:
+		empHrs = 0;
+		}	
+    empWage = empHrs * WAGE_PER_HRS;
+    System.out.println("Employee wage : " + empWage);
+    }
+}
